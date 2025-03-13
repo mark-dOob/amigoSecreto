837347c5ccd229a1d4488b4f48459df91ad4dc2e
mark-dOob <201265178+mark-dOob@users.noreply.github.com>
2025-03-13 12:08:38 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,8 @@
+/*
+Autor: Mark-dOob
+Fecha: 11-03-2025
+Hora: 16:31
+*/
+
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
